1fd3eaf043dcc4eb0b4df15df2c3e8371c27f2b7
Surakarn Samkaew <tonkla@gmail.com>
2022-04-30 19:24:04 +0700

    Increase interval fetching getOpenPositions() to 10s

--- robot/fttt/fttt-1.ts
+++ robot/fttt/fttt-1.ts
@@ -229,7 +229,7 @@ async function main() {
   const id6 = setInterval(() => fetchBookTickers(), 4000) // 4s
 
   await getOpenPositions()
-  const id7 = setInterval(() => getOpenPositions(), 5000) // 5s
+  const id7 = setInterval(() => getOpenPositions(), 10000) // 10s
 
   gracefulShutdown([id1, id2, id3, id4, id5, id6, id7])
 }
