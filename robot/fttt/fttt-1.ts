35376e25316fc1c946d9bf953ae73604cf7b9b06
Surakarn Samkaew <tonkla@gmail.com>
2022-05-01 14:59:22 +0700

    Sync interval time

--- robot/fttt/fttt-1.ts
+++ robot/fttt/fttt-1.ts
@@ -247,7 +247,7 @@ async function main() {
   const id6 = setInterval(() => fetchBookTickers(), 4000) // 4s
 
   await getOpenPositions()
-  const id7 = setInterval(() => getOpenPositions(), 10000) // 10s
+  const id7 = setInterval(() => getOpenPositions(), 5000) // 5s
 
   gracefulShutdown([id1, id2, id3, id4, id5, id6, id7])
 }
