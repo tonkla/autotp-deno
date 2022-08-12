525f204587452643e8a261e9ad2ed37af1ade98b
Surakarn Samkaew <tonkla@gmail.com>
2022-08-12 18:53:58 +0700

    Tune

--- robot/hlma/trend.ts
+++ robot/hlma/trend.ts
@@ -17,11 +17,11 @@ function Trend(ta: TaValues): ITrend {
   }
 
   function isUpCandle() {
-    return ta.hc_0 < 0.2 && (ta.hl_0 > 0.5 || (ta.hl_0 > 0.2 && ta.co_0 > 0))
+    return ta.hc_0 < 0.2 && ta.co_0 > 0.1
   }
 
   function isDownCandle() {
-    return ta.cl_0 < 0.2 && (ta.hl_0 > 0.5 || (ta.hl_0 > 0.2 && ta.co_0 < 0))
+    return ta.cl_0 < 0.2 && ta.co_0 < -0.1
   }
 
   return {
