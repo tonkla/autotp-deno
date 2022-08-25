0cc06c5572ad2c5af6769e1f7b097151077e8f25
Surakarn Samkaew <tonkla@gmail.com>
2022-08-25 20:01:44 +0700

    Tune

--- robot/hlma/finder-b.ts
+++ robot/hlma/finder-b.ts
@@ -63,7 +63,7 @@ const Finder = ({ config, symbols, db, redis, exchange }: ExtBotProps) => {
       const { tah, info, markPrice } = p
 
       if (tah.hsl_0 < -0.1 && tah.lsl_0 < 0) continue
-      if (markPrice > tah.cma_0 - tah.atr * 0.3) continue
+      if (markPrice > tah.cma_0 - tah.atr * 0.4) continue
 
       const siblings = await db.getSiblingOrders({
         symbol,
@@ -110,7 +110,7 @@ const Finder = ({ config, symbols, db, redis, exchange }: ExtBotProps) => {
       const { tah, info, markPrice } = p
 
       if (tah.hsl_0 > 0 && tah.lsl_0 > 0.1) continue
-      if (markPrice < tah.cma_0 + tah.atr * 0.3) continue
+      if (markPrice < tah.cma_0 + tah.atr * 0.4) continue
 
       const siblings = await db.getSiblingOrders({
         symbol,
@@ -166,7 +166,7 @@ const Finder = ({ config, symbols, db, redis, exchange }: ExtBotProps) => {
 
       if (await db.getStopOrder(o.id, OrderType.FTP)) continue
 
-      const shouldSl = tah.lsl_0 < 0.02 && minutesToNow(o.openTime) > 5
+      const shouldSl = tah.lsl_0 < 0.02 && minutesToNow(o.openTime) > 10
       const slMin = tah.atr * config.slMinAtr
       if ((slMin > 0 && o.openPrice - markPrice > slMin) || shouldSl) {
         const order = await buildLongSLMakerOrder(o)
@@ -214,7 +214,7 @@ const Finder = ({ config, symbols, db, redis, exchange }: ExtBotProps) => {
 
       if (await db.getStopOrder(o.id, OrderType.FTP)) continue
 
-      const shouldSl = tah.hsl_0 > -0.02 && minutesToNow(o.openTime) > 5
+      const shouldSl = tah.hsl_0 > -0.02 && minutesToNow(o.openTime) > 10
       const slMin = tah.atr * config.slMinAtr
       if ((slMin > 0 && markPrice - o.openPrice > slMin) || shouldSl) {
         const order = await buildShortSLMakerOrder(o)
