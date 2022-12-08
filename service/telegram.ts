038d24abb7381f2196c8197ae646df1d55b0686f
Surakarn Samkaew <tonkla@gmail.com>
2022-12-08 08:45:29 +0700

    Add try/catch

--- service/telegram.ts
+++ service/telegram.ts
@@ -4,14 +4,18 @@ export async function sendMessage(
   text: string,
   markdown?: boolean
 ) {
-  const url = `https://api.telegram.org/bot${botToken}/sendMessage`
-  const body: { chat_id: string; text: string; parse_mode?: string } = { chat_id: chatId, text }
-  if (markdown) body.parse_mode = 'MarkdownV2'
-  await fetch(url, {
-    method: 'POST',
-    headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
-    body: JSON.stringify(body),
-  })
+  try {
+    const url = `https://api.telegram.org/bot${botToken}/sendMessage`
+    const body: { chat_id: string; text: string; parse_mode?: string } = { chat_id: chatId, text }
+    if (markdown) body.parse_mode = 'MarkdownV2'
+    await fetch(url, {
+      method: 'POST',
+      headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export default {
