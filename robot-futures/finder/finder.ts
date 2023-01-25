5b6184dcb9c11235517e85e8370b872fe08661ab
Surakarn Samkaew <tonkla@gmail.com>
2023-01-25 20:57:54 +0700

    Tune

--- robot-futures/finder/finder.ts
+++ robot-futures/finder/finder.ts
@@ -6,12 +6,13 @@ import { PrivateApi } from '../../exchange/binance/futures.ts'
 import type { BotFunc } from '../../types/index.ts'
 import { Config, getConfig } from './config.ts'
 import Finder1 from './finder-1.ts'
+import Finder2 from './finder-2.ts'
 
 async function finder() {
   try {
     const env = dotenv.config()
 
-    const bots: BotFunc[] = [Finder1]
+    const bots: BotFunc[] = [Finder1, Finder2]
 
     const config: Config = await getConfig()
 
